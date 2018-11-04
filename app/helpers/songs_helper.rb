f75da597cefa0a3d05a220d12c34e40a4617683d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-04 20:19:02 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def display_artist(song)
     if song.artist_name
-      link_to song.artist, artist_path(song.artist)
+      link_to song.artist, artist_path(song.artist.id)
     else
       link_to "Add Artist", song_path(song)
     end
